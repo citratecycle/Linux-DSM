98941746c2986cec4fc74046301ef67639a1dadb
citrate <citrate@sjtu.edu.cn>
2022-09-27 21:59:41 +0800

    fix: fix deadlock caused by small KTCP buffer

--- arch/x86/kvm/ktcp.c
+++ arch/x86/kvm/ktcp.c
@@ -55,7 +55,8 @@ struct ktcp_cb
 	struct socket *socket;
 };
 
-#define KTCP_BUFFER_SIZE (sizeof(struct ktcp_hdr) + PAGE_SIZE)
+// #define KTCP_BUFFER_SIZE (sizeof(struct ktcp_hdr) + PAGE_SIZE)
+#define KTCP_BUFFER_SIZE (sizeof(struct ktcp_hdr) + (1 + 2 + PAGE_SIZE + (16 + PAGE_SIZE) * KVM_PREFETCH_MAX_WINDOW_SIZE))
 
 static int __ktcp_send(struct socket *sock, const char *buffer, size_t length,
 		unsigned long flags)
